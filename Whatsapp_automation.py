86507cbccf1e4a874486219d419b6fbcd451ef3b
XavierSng <135297465+XavierSng@users.noreply.github.com>
2023-06-24 21:48:49 +0800

    Update Whatsapp_automation.py
    Updated

--- Whatsapp_automation.py
+++ Whatsapp_automation.py
@@ -1,4 +1,4 @@
-# Program to send bulk customized message through WhatsApp web application
+""" Program to send bulk customized message through WhatsApp web application"""
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,7 +8,6 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
 import pandas
 import time
-import os
 
 # Load the chrome driver
 driver = webdriver.Chrome()
@@ -16,33 +15,17 @@ count = 0
 
 # Open WhatsApp URL in Chrome browser
 driver.get("https://web.whatsapp.com/")
-input('Scanning of QR Code')
+input('Scanning of QR Code, press enter when done.')
 wait = WebDriverWait(driver, 10)
 
 # Read data from excel
 excel_data = pandas.read_excel('Customer bulk email data.xlsx', sheet_name='Customers')
 
 # Iterate excel rows till to finish
-for column in excel_data['Name'].tolist():
+for column in excel_data['Contact'].tolist():
     # Assign customized message
     message = """
-Low Levy Myanmar Construction Workers (R1)
 
-Dear Sir Sir/ Madam,
-
-We supply low levy fresh Myanmar Construction workers with BCA Certificate (SECK - R1) trained in formwork and pipe fitting
-
-- Workers with SEC(K) - R1 Cert
-- Fast arrival (Less than 2 weeks)
-- Low levy
-- No agency fees
-
-Pls contact me if you are keen to employ them.
-
-Thanks & regards,
-Eileen Siah 
-Times Recruitment Pte Ltd
-MOM License 16C7960
 """
 
     # Locate search box through x_path
@@ -56,7 +39,7 @@ MOM License 16C7960
     person_title.send_keys(str(excel_data['Contact'][count]))
     count = count + 1
 
-    # Wait for 5 seconds to search contact number
+    # Wait for 5 seconds to search contact number, can shorten
     time.sleep(5)
 
     try:
@@ -76,19 +59,25 @@ MOM License 16C7960
             chatbox.send_keys(part)
             ActionChains(driver).key_down(Keys.SHIFT).key_down(Keys.ENTER).key_up(Keys.SHIFT).key_up(
                 Keys.ENTER).perform()
+
+        # Time to check message, can shorten
+        time.sleep(5)
         chatbox.send_keys(Keys.ENTER)
 
         attach = driver.find_element(By.XPATH, "//span[@data-icon ='clip']")
         attach.click()
 
+        # Time to open gallery, might be unnecessary(?)
         time.sleep(3)
         gallery = wait.until(lambda driver: driver.find_element(By.XPATH, "//input[@accept='image/*,video/mp4,video/3gpp,video/quicktime']"))
         gallery.send_keys(r"C:\Users\sngxa\PycharmProjects\pythonProject\Construction.jpg")
 
+        # Time to open image send portion, necessary **CANNOT** shorten
         time.sleep(3)
         actions = ActionChains(driver)
         actions.send_keys(Keys.ENTER)
         actions.send_keys(Keys.ENTER)
         actions.perform()
-        time.sleep(3)
 
+# Loading of last image sent, **CANNOT** shorten
+time.sleep(3)
